01626f5c63b72abcd3a9182a6b798f557f614974
Nichole Emmons <bemmons1@asu.edu>
2017-11-22 15:36:24 -0600

    Making lots of progress on github api calls and photos

--- website/views.py
+++ website/views.py
@@ -2,14 +2,20 @@ from __future__ import unicode_literals
 from datetime import *
 from PIL import Image
 from django.shortcuts import render
-from klphotography import settings
 import requests
-import json
+import json as JSON
 import urllib2 as urllib
 import io
 from collections import namedtuple
+from django.core.cache import cache
+from copy import copy
 
 categories = {'pets', 'portraits', 'automotive', 'misc', 'weddings'}
+
+repo = 'kl-photography-media'
+owner = 'everwoodlove'
+
+BASE_URL = 'https://api.github.com/repos/' + owner + '/' + repo + '/contents/'
 
 def home(request):
     photos = get_homepage_photos_from_git()
@@ -20,38 +26,34 @@ def home(request):
 
     return render(request, 'website/home.html', context)
 
+
 def pricing(request):
     return render(request, 'website/pricing.html')
+
 
 def about(request):
     return render(request, 'website/about-me.html')
 
+
 def contact(request):
     return render(request, 'website/contact.html')
 
+
 def shoot(request, category, date, name):
-    # Build URL from params
-    # get all images in the folder, including in the preview folder
-    # return them
+    url = BASE_URL + category + '/' + date + '/' + name
 
-    #TODO
-    url = settings.STATIC_URL + '/website/photos/' + category + '/' + date + '/' + name
+    all_photos = get_all_photoshoot_photos(url)
 
-    landscapes, portraits = get_all_photos(url)
-    landscape_photos = []
-    portrait_photos = []
+    photos_to_use = []
 
-    for photo in landscapes:
-        photo = photo.replace('/static/', '')
-        landscape_photos.append(photo)
+    for photo in all_photos:
+        photo_metadata = build_photo_information(photo)
+        photos_to_use.append(photo_metadata)
 
-    for photo in portraits:
-        photo = photo.replace('/static/', '')
-        portrait_photos.append(photo)
-
-    context = {'landscapes': landscape_photos, 'portraits': portrait_photos}
+    context = {'photos': photos_to_use}
 
     return render(request, 'website/shoot.html', context)
+
 
 def gallery(request, category):
     url = 'website/gallery/'
@@ -236,14 +238,13 @@ def gallery(request, category):
 def get_homepage_photos_from_git():
     homepage_photos = []
 
-    preview_photos = get_newest_preview_photoshoot_photos_from_git()
-    photo_metadata = build_photo_information(preview_photos)
-    homepage_photos.append(photo_metadata)
+    preview_photos = get_newest_photoshoot_photos_from_git(True)
 
-    landscape_photos, portrait_photos = sort_photos_by_aspect(homepage_photos)
-    sorted_photos = {'landscapes': landscape_photos, 'portraits': portrait_photos}
+    for photo in preview_photos:
+        photo_metadata = build_photo_information(photo)
+        homepage_photos.append(photo_metadata)
 
-    homepage_photos = assign_numbers_to_photos(sorted_photos)
+    homepage_photos = assign_numbers_to_photos(homepage_photos)
 
     return homepage_photos
 
@@ -255,13 +256,21 @@ def build_photo_information(photo):
 
     if photo:
         shoot_name, shoot_date, category = parse_photo_path(photo)
-        orientation = determine_photo_orientation() #TODO
+        orientation = determine_photo_orientation(photo)
 
-        photo_metadata['shoot_name'] = shoot_name
-        photo_metadata['shoot_date'] = shoot_date
-        photo_metadata['category'] = category
-        photo_metadata['orientation'] = orientation
-        photo_metadata['url'] = photo.url
+        # photo_metadata['shoot_name'] = shoot_name
+        # photo_metadata['shoot_date'] = shoot_date
+        # photo_metadata['category'] = category
+        # photo_metadata['orientation'] = orientation
+        # photo_metadata['url'] = photo.downloadurl
+
+        photo_metadata = {
+            'shoot_name': shoot_name,
+            'shoot_date': shoot_date,
+            'category': category,
+            'orientation': orientation,
+            'url': photo.downloadurl
+        }
 
     return photo_metadata
 
@@ -273,22 +282,36 @@ def build_photo_information(photo):
 # 2 = half width, landscape or portrait
 def assign_numbers_to_photos(photo_list):
     i = 0
+    landscape_list_index = 0
+    portrait_list_index = 0
+
+    list_length = 10
+
+    landscape_photos, portrait_photos = sort_photos_by_aspect(photo_list)
+
+    landscape_length = len(landscape_photos)
+    portrait_length = len(portrait_photos)
+
     assigned_photos = []
 
-    for photo in photo_list:
+    for x in xrange(list_length):
         if i == 0:
-            orientation_type = 'landscapes'
-        else:
-            orientation_type = 'portraits'
+            if landscape_list_index < landscape_length:
+                new_photo = copy(landscape_photos[landscape_list_index])
+                landscape_list_index += 1
+                new_photo['block'] = i
+                assigned_photos.append(new_photo)
+                i += 1
+        elif i == 1 or i == 2:
+            if portrait_list_index < portrait_length:
+                new_photo = copy(portrait_photos[portrait_list_index])
+                new_photo['block'] = i
+                assigned_photos.append(new_photo)
 
-        if photo.get(orientation_type, []):
-            photo['block'] = i
-            i += 1
+                i += 1
 
-            if i > 2:
-                i = 0
-
-            assigned_photos.append(photo)
+                if i > 2:
+                    i = 0
 
     return assigned_photos
 
@@ -298,8 +321,8 @@ def assign_numbers_to_photos(photo_list):
 def parse_photo_path(photo):
     path_separated = photo.path.split('/')
 
-    shoot_name = path_separated[1]
-    shoot_date = path_separated[2].split('.')
+    shoot_name = path_separated[2]
+    shoot_date = path_separated[1]
     shoot_category = path_separated[0]
 
     return shoot_name, shoot_date, shoot_category
@@ -309,7 +332,7 @@ def parse_photo_path(photo):
 # to determine if the image is portrait style or landscape style
 # in terms of its orientation.
 def determine_photo_orientation(photo):
-    file_data = urllib.urlopen(photo.url)
+    file_data = urllib.urlopen(photo.downloadurl)
     image_file = io.BytesIO(file_data.read())
     image = Image.open(image_file)
     width, height = image.size
@@ -324,18 +347,16 @@ def determine_photo_orientation(photo):
 # It is a list of preview photos from the most recent photoshoots
 # The number of photoshoots looked at is determined by "list_length"
 # The category the photoshoots are pulled from is determined by "category"
-def get_newest_preview_photoshoot_photos_from_git():
-    repo = 'kl-photography-media'
-    owner = 'everwoodlove'
+def get_newest_photoshoot_photos_from_git(only_get_preview_photos = False):
     photos_to_return = []
 
     # 5 categories will return a max of 15 photo shoots
     max_photoshoots_per_category = 3
 
     for category in categories:
-        get_url = 'https://api.github.com/repos/' + owner + '/' + repo + '/contents/' + category + '/'
+        get_url = BASE_URL + category + '/'
 
-        repo_contents_array = requests.get(get_url)
+        repo_contents_array = get_api_call_contents(get_url)
         photo_date_directories = parse_json_array(repo_contents_array, 'dir')
 
         newest_photoshoot_dates = get_newest_dates(photo_date_directories, max_photoshoots_per_category)
@@ -343,19 +364,34 @@ def get_newest_preview_photoshoot_photos_from_git():
         if newest_photoshoot_dates:
             for date in newest_photoshoot_dates:
                 photoshoot_get_url = get_url + date + '/'
-                photoshoot_directories = requests.get(photoshoot_get_url)
+                photoshoot_directories = get_api_call_contents(photoshoot_get_url)
                 photoshoot_directories = parse_json_array(photoshoot_directories, 'dir')
 
                 # Get all the photoshoots for that date
                 # And get all the preview photos for those photoshoots
                 for photo_shoot in photoshoot_directories:
-                    preview_photos_json = requests.get(photoshoot_get_url + photo_shoot.name + '/preview')
-                    preview_photos = parse_json_array(preview_photos_json, 'file')
+                    preview_photos_json = get_api_call_contents(photoshoot_get_url + photo_shoot.name + '/preview')
+                    preview_photos = parse_json_array(preview_photos_json, 'file', '.jpg')
 
-                    photos_to_return.append(preview_photos)
+                    regular_photos_json = get_api_call_contents(photoshoot_get_url + photo_shoot.name)
+                    regular_photos = parse_json_array(regular_photos_json, 'file', '.jpg')
 
+                    if only_get_preview_photos:
+                        photos_to_return  = photos_to_return + preview_photos
+                    else:
+                        photos_to_return = photos_to_return + preview_photos + regular_photos
 
     return photos_to_return
+
+
+def get_all_photoshoot_photos(url):
+    photoshoot_photos = get_api_call_contents(url)
+    preview_photoshoot_photos = get_api_call_contents(url+ '/preview')
+
+    regular_photos = parse_json_array(photoshoot_photos, 'file', '.jpg')
+    preview_photos = parse_json_array(preview_photoshoot_photos, 'file', '.jpg')
+
+    return regular_photos + preview_photos
 
 
 # This function returns a list of date strings for the newest dates
@@ -400,13 +436,12 @@ def get_newest_dates(dated_folder_objects, max_folders):
 
 
 def sort_photos_by_aspect(photo_list):
-    # Want to determine portrait/landscape
-    # assign to portrait or landscape list on the
     landscape_photos = []
     portrait_photos = []
 
     for photo in photo_list:
-        if photo.orientation == 'landscape':
+        type = photo.get('orientation')
+        if str(type) == 'landscape':
             landscape_photos.append(photo)
         else:
             portrait_photos.append(photo)
@@ -415,25 +450,53 @@ def sort_photos_by_aspect(photo_list):
 
 
 def _json_object_hook(d):
+    for key in d.keys():
+        new_key = key.replace("_", "")
+        if new_key != key:
+            d[new_key] = d[key]
+            del d[key]
+
     return namedtuple('X', d.keys())(*d.values())
 
 
-def json2obj(data):
-    return json.loads(data, object_hook=_json_object_hook)
-
-
-def parse_json_array(json_array, selector_type):
+def parse_json_array(json_array, selector_type = None, name_filter = None):
     array = []
+    json_array = JSON.loads(json_array, object_hook=_json_object_hook)
 
     for json in json_array:
-        object = json2obj(json)
-
         if selector_type:
-            if object.type == selector_type:
-                array.append(object)
+            if json.type == selector_type:
+                if name_filter:
+                    if name_filter in json.name:
+                        array.append(json)
+                else:
+                    array.append(json)
         else:
-            array.append(object)
+            array.append(json)
 
     return array
+
+
+def get_api_call_contents(url):
+    value = cache.get(url)
+
+    if value:
+        return value
+    else:
+        # TODO do not deploy this!!!!!!
+        new_value = requests.get(url, auth=('everwoodlove', '887e3331571aa967e9f5a802827002e44788ea7b')).content
+        cache.set(url, new_value)
+
+        return new_value
+
+
+def get_from_cache(key):
+    return cache.get(key)
+
+
+def set_in_cache(key, value):
+    cache.set(key, value)
+
+
 
 
