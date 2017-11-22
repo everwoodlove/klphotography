481cc159b81ecb75540fa7d166030c1f212931b3
Nichole Emmons <bemmons1@asu.edu>
2017-11-22 15:55:51 -0600

    Getting gallery pages done

--- website/views.py
+++ website/views.py
@@ -10,12 +10,13 @@ from collections import namedtuple
 from django.core.cache import cache
 from copy import copy
 
-categories = {'pets', 'portraits', 'automotive', 'misc', 'weddings'}
+CATEGORIES = {'pets', 'portraits', 'automotive', 'misc', 'weddings'}
 
-repo = 'kl-photography-media'
-owner = 'everwoodlove'
+REPO = 'kl-photography-media'
+OWNER = 'everwoodlove'
 
-BASE_URL = 'https://api.github.com/repos/' + owner + '/' + repo + '/contents/'
+BASE_URL = 'https://api.github.com/repos/' + OWNER + '/' + REPO + '/contents/'
+
 
 def home(request):
     photos = get_homepage_photos_from_git()
@@ -58,187 +59,29 @@ def shoot(request, category, date, name):
 def gallery(request, category):
     url = 'website/gallery/'
 
-    for cat in categories:
+    for cat in CATEGORIES:
         if category == cat:
             url += cat + '.html'
             break
 
-    context = {'photos': get_preview_photos_from(category, 3)}
+    photos = get_newest_photoshoot_photos_from_git(category_given=category)
+    total_photos = []
+
+    for photo in photos:
+        photo_metadata = build_photo_information(photo)
+        total_photos.append(photo_metadata)
+
+    total_photos = assign_numbers_to_photos(total_photos)
+
+    context = {'photos': total_photos}
 
     return render(request, url, context)
 
-# Its ideal to have landscape photos be the full images
-# and portraits to be the half images
-# But what if we only have 1 portrait and 2 landscapes?
-
-#TODO for home photos:
-# parse through all directories and get most recent 6-7 shoots to display and pick first image of each in the preview folder
-# def get_homepage_photos():
-#     homepage_photos = []
-#     i = 0
-#
-#     for category in categories:
-#         photo_set = get_preview_photos(category, 7)
-#         for photo in photo_set:
-#             if i == 0:
-#                 name = 'landscapes'
-#             else:
-#                 name = 'portraits'
-#
-#             if photo.get(name, []):
-#                 date = photo.get('date', '')
-#                 date_split = date.split('.')
-#
-#                 if (len(date_split) is 3):
-#                     day = date_split[1]
-#                     month = date_split[0]
-#                     year = date_split[2]
-#                 else:
-#                     day = ''
-#                     month = ''
-#                     year = ''
-#
-#                 shoot_photo = {'name': photo.get('name', ''), 'day': day, 'month': month, 'year': year,
-#                                'photo': photo.get(name, [''])[0],
-#                                'category': photo.get('category', ''), 'number': i}
-#                 i += 1
-#
-#                 if i > 2:
-#                     i = 0
-#
-#                 homepage_photos.append(shoot_photo)
-#
-#     return homepage_photos
-
-# def get_preview_photos(category, list_length):
-#     #string = 'website/photos/' + category
-#     original_path = os.path.join(settings.STATIC_ROOT, 'app/a.txt')
-#     #os.path.join(static(string))
-#     print original_path
-#     dates_directories = get_most_recent_folders_by_date(original_path)
-#     photos_to_return = []
-#
-#     if dates_directories:
-#         for date in dates_directories:
-#             folder_path = original_path + '/' + date
-#             shoot_name_directories = os.listdir(folder_path)
-#
-#             for shoot_name in shoot_name_directories:
-#                 path = folder_path + '/' + shoot_name + '/preview'
-#                 preview_photos = get_images(path)
-#
-#                 landscapes, portraits = sort_images(preview_photos)
-#
-#                 photo_set = {'name': shoot_name, 'date': date, 'landscapes': landscapes, 'portraits': portraits, 'category': category}
-#                 photos_to_return.append(photo_set)
-#
-#                 if len(photos_to_return) > list_length:
-#                     break
-#
-#             if len(photos_to_return) > list_length:
-#                 break
-#
-#     return photos_to_return
-#
-# def get_all_photos(path):
-#     photo_set = []
-#
-#     if os.path.isdir(path):
-#         photos = get_images(path)
-#         photo_set = photo_set + photos
-#
-#     path = path + '/preview'
-#
-#     if os.path.isdir(path):
-#         photos = get_images(path)
-#         photo_set = photo_set + photos
-#
-#     landscapes, portraits = sort_images(photo_set)
-#
-#     return landscapes, portraits
-#
-# def get_most_recent_folders_by_date(path):
-#     if os.path.isdir(path):
-#         directory_names = os.listdir(path)
-#
-#         dates_list = []
-#
-#         for name in directory_names:
-#             date_split = name.split('.')
-#
-#             if (len(date_split) is 3):
-#                 folderDate = datetime(int(date_split[2]), int(date_split[0]), int(date_split[1]))
-#                 dates_list.append(folderDate)
-#
-#         folders_to_use = []
-#         now = datetime.today()
-#
-#         list_length = len(dates_list)
-#
-#
-#         for i in xrange(list_length):
-#             youngest = max(dt for dt in dates_list if dt < now)
-#
-#             folders_to_use.append(youngest)
-#             dates_list.remove(youngest)
-#
-#         folders = []
-#
-#         for i in xrange(list_length):
-#             folders.append(folders_to_use[i].strftime('%m.%d.%Y'))
-#
-#         return folders
-#     else:
-#         return []
-#
-# def get_images(path):
-#     photos = []
-#
-#     only_files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-#
-#     for file in only_files:
-#         if file.endswith(".png") or file.endswith(".jpg"):
-#             photo_path = os.path.join(path, file)
-#             photos.append(photo_path)
-#
-#     return photos
-#
-# def sort_images(images_list):
-#     landscapes = []
-#     portraits = []
-#
-#     for image in images_list:
-#         im = Image.open(image)
-#         width, height = im.size
-#
-#         # TODO remove this line when going to prod
-#         image = image.replace('/static/', '')
-#         if width > height:
-#             landscapes.append(image)
-#         else:
-#             portraits.append(image)
-#
-#     return landscapes, portraits
-
-
-
-
-
-
-
-
-
-
-
-
-# This gets 7 photo shoots worth of preview photos to display on the homepage.
-# The list returned should be a list of objects that contain the photo's shoot name,
-# category, date (in day/month/year separately), url, and number.
 
 def get_homepage_photos_from_git():
     homepage_photos = []
 
-    preview_photos = get_newest_photoshoot_photos_from_git(True)
+    preview_photos = get_newest_photoshoot_photos_from_git(only_get_preview_photos=True)
 
     for photo in preview_photos:
         photo_metadata = build_photo_information(photo)
@@ -347,13 +190,18 @@ def determine_photo_orientation(photo):
 # It is a list of preview photos from the most recent photoshoots
 # The number of photoshoots looked at is determined by "list_length"
 # The category the photoshoots are pulled from is determined by "category"
-def get_newest_photoshoot_photos_from_git(only_get_preview_photos = False):
+def get_newest_photoshoot_photos_from_git(category_given = None, only_get_preview_photos = False):
     photos_to_return = []
 
     # 5 categories will return a max of 15 photo shoots
     max_photoshoots_per_category = 3
 
-    for category in categories:
+    categories_to_use = CATEGORIES
+
+    if category_given:
+        categories_to_use = [category_given]
+
+    for category in categories_to_use:
         get_url = BASE_URL + category + '/'
 
         repo_contents_array = get_api_call_contents(get_url)
@@ -484,7 +332,7 @@ def get_api_call_contents(url):
         return value
     else:
         # TODO do not deploy this!!!!!!
-        new_value = requests.get(url, auth=('everwoodlove', '887e3331571aa967e9f5a802827002e44788ea7b')).content
+        new_value = requests.get(url, auth=('everwoodlove', '7c57bf5eaed676db5ed7da3a27980243780c5cbb')).content
         cache.set(url, new_value)
 
         return new_value
